e9a229a36995b82a28112e38a565e98549a630d7
Ryo Nakamura <ryo-nakamura@cookpad.com>
2014-04-25 21:12:21 +0900

    bundle gem ellen-github

---
+++ lib/ellen/github.rb
@@ -0,0 +1,7 @@
+require "ellen/github/version"
+
+module Ellen
+  module Github
+    # Your code goes here...
+  end
+end
---
+++ lib/ellen/github/version.rb
@@ -0,0 +1,5 @@
+module Ellen
+  module Github
+    VERSION = "0.0.1"
+  end
+end
